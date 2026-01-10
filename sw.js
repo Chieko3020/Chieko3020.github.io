50c9c65e9b7945e4848c027d1cfdc1b21c820c06
Chieko3020 <rintsuki@qq.com>
2026-01-10 08:26:08 +0800

    Auto deploy: YYYY-01-2026年1月10日 08:25:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760519842318";const preCache = ["/images/taichi.png","/images/bg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768004746526";const preCache = ["/images/taichi.png","/images/bg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
